5723f073b5335f39e46baecff0ca5d3e66618557
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-03 22:42:45 +0530

    Setup Express

--- coders-united-backend/index.js
+++ coders-united-backend/index.js
@@ -1,5 +1,20 @@
-const startBot = require("./config/bot-config.js");
 require("dotenv").config();
+const startBot = require("./config/bot-config.js");
+const express = require("express");
+
 const Bot_Token = process.env.TOKEN;
 
 startBot(Bot_Token);
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api Runs...");
+});
+
+app.listen(process.env.PORT || 5050, () => {
+  console.log(
+    `Server Listening at ${process.env.PORT || 5050} on ${
+      process.env.NODE_ENV
+    } mode`
+  );
+});
