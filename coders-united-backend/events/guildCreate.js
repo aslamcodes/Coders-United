a0ee74a808ae57ffd94685bfa03b2b92d934e13e
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-06 23:08:22 +0530

    GuildCreate Event

--- coders-united-backend/events/guildCreate.js
+++ coders-united-backend/events/guildCreate.js
@@ -1,14 +1,18 @@
 const { GUILD_TEXT } = require("../constants/channel_types.js");
 const deploy_commands = require("../utils/depoy-commands.js");
-
+const fs = require("fs");
+const path = require("path");
 module.exports = {
   name: "guildCreate",
   async execute(interaction) {
     deploy_commands(interaction.id);
-    interaction.channels.cache
-      .filter((channel) => channel.type === GUILD_TEXT)
-      .each((channel) => {
-        console.log(channel);
-      });
+    fs.writeFileSync(
+      path.resolve(__dirname, "../data/db.json"),
+      JSON.stringify({
+        "Text Channels": interaction.channels.cache.filter(
+          (channel) => channel.type === GUILD_TEXT
+        ),
+      })
+    );
   },
 };
