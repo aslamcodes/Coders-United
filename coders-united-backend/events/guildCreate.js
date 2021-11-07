fbf8f95b403ec7e372ffe74d3b26dbbd4dbda530
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-07 18:09:53 +0530

    Add to database on GuildCreate

--- coders-united-backend/events/guildCreate.js
+++ coders-united-backend/events/guildCreate.js
@@ -2,18 +2,25 @@ const { GUILD_TEXT } = require("../constants/channel_types.js");
 const deploy_commands = require("../utils/depoy-commands.js");
 const fs = require("fs");
 const path = require("path");
+const Channel = require("../models/Channel.js");
+const { channel } = require("diagnostics_channel");
 
 module.exports = {
   name: "guildCreate",
   async execute(interaction) {
     deploy_commands(interaction.id);
-    fs.writeFileSync(
-      path.resolve(__dirname, "../data/channels.json"),
-      JSON.stringify({
-        "Text Channels": interaction.channels.cache.filter(
-          (channel) => channel.type === GUILD_TEXT
-        ),
-      })
-    );
+
+    const channels = interaction.channels.cache
+      .filter((channel) => channel.type === GUILD_TEXT)
+      .map((channel) => {
+        return {
+          type: channel.type,
+          name: channel.name,
+          id: channel.id,
+          guildId: channel.guildId,
+        };
+      });
+
+    await Channel.insertMany(channels);
   },
 };
