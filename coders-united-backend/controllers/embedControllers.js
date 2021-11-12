ed9528078ea975610a488e769aa44c5c5f16f333
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-12 21:05:37 +0530

    Embed  backend

--- coders-united-backend/controllers/embedControllers.js
+++ coders-united-backend/controllers/embedControllers.js
@@ -1,11 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const asyncHandler = require("express-async-handler");
+
 // @desc Send an embed to a channel by ID
 // @route POST /channels
 // @access private
 // @param embed:object, channelId:string
 const sendEmbedToChannel = asyncHandler(async (req, res) => {
-  const { message, channelId } = req.body;
+  const { channelId } = req.body;
   const client = require("..");
   const channel = client.channels.cache.get(channelId);
-  channel.send(message);
-  res.send("Posting a Message");
+
+  const Embed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("Some title")
+    .setURL("https://discord.js.org/")
+    .setAuthor(
+      "Some name",
+      "https://i.imgur.com/AfFp7pu.png",
+      "https://discord.js.org"
+    )
+    .setDescription("Some description here")
+    .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+    .addFields(
+      { name: "Regular field title", value: "Some value here" },
+      { name: "\u200B", value: "\u200B" },
+      { name: "Inline field title", value: "Some value here", inline: true },
+      { name: "Inline field title", value: "Some value here", inline: true }
+    )
+    .addField("Inline field title", "Some value here", true)
+    .setImage("https://i.imgur.com/AfFp7pu.png")
+    .setTimestamp()
+    .setFooter("Some footer text here", "https://i.imgur.com/AfFp7pu.png");
+  try {
+    await channel.send({ embeds: [Embed] });
+  } catch (error) {
+    throw new Error(error);
+  }
+
+  res.send("Message Sent");
 });
+
+module.exports = {
+  sendEmbedToChannel,
+};
--- coders-united-backend/routes/channelRoutes.js
+++ coders-united-backend/routes/channelRoutes.js
@@ -3,12 +3,18 @@ const {
   getChannels,
   sendMessageToChannel,
 } = require("../controllers/channelControllers");
+const { sendEmbedToChannel } = require("../controllers/embedControllers");
+
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").get(protect, getChannels).post(protect, sendMessageToChannel);
-
+router
+  .route("/")
+  .get(protect, getChannels)
+  .post(protect, sendMessageToChannel)
+  .put(protect, sendEmbedToChannel);
+router.route("/embed").post(protect, sendEmbedToChannel);
 module.exports = {
   channelRouter: router,
 };
