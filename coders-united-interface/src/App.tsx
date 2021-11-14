200f83a2e8004e45597d5e1c50151080700f2715
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-14 19:58:32 +0530

    Add Footer to App

--- coders-united-interface/src/App.tsx
+++ coders-united-interface/src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import { RequireAuth } from "./Components/Auth/RequireAuth";
+import { Container } from "./Components/Helpers/Container";
+import Footer from "./Components/UI/Footer";
 import { Navbar } from "./Components/UI/Navbar";
 import { Home } from "./Pages";
 import { Login } from "./Pages/Login";
@@ -11,14 +13,19 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route element={<RequireAuth />}>
-          <Route path="/admin/send-message" element={<SendMessage />} />
-          <Route path="/admin/send-embed" element={<SendEmbed />} />
-        </Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Container>
+        <main>
+          <Routes>
+            <Route element={<RequireAuth />}>
+              <Route path="/admin/send-message" element={<SendMessage />} />
+              <Route path="/admin/send-embed" element={<SendEmbed />} />
+            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+      </Container>
+      <Footer />
     </Router>
   );
 }
