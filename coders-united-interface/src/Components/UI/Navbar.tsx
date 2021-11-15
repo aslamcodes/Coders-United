9942b15eae771c9b4a6acf15c7714b3deeee84e1
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-15 17:52:32 +0530

    Remove login after login for non admins

--- coders-united-interface/src/Components/UI/Navbar.tsx
+++ coders-united-interface/src/Components/UI/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useAuthContext } from "../../Context/Authentication/AuthContext";
 import { Button } from "./Button";
 import styles from "./Navbar.module.css";
 export const Navbar = () => {
   const { user } = useAuthContext();
+  const { pathname } = useLocation();
 
   return (
     <header>
@@ -18,10 +19,12 @@ export const Navbar = () => {
           <h1>Coders United</h1>
         </div>
         <div className={styles["nav-actions"]}>
-          {user && user.isAdmin ? (
-            <Link to="admin">
-              <Button variant="secondary">Admin Actions</Button>
-            </Link>
+          {user ? (
+            user.isAdmin ? (
+              <Link to="admin">
+                <Button variant="secondary">Admin Actions</Button>
+              </Link>
+            ) : null
           ) : (
             <Link to="/login">
               <Button variant="secondary">Login</Button>
