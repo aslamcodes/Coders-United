239da3ee727f3e7815100a2e660e49c58d1083fd
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-15 10:27:07 +0530

    Remove footer and add admin page

--- coders-united-interface/src/App.tsx
+++ coders-united-interface/src/App.tsx
@@ -2,9 +2,9 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import { RequireAuth } from "./Components/Auth/RequireAuth";
 import { Container } from "./Components/Helpers/Container";
-import Footer from "./Components/UI/Footer";
 import { Navbar } from "./Components/UI/Navbar";
 import { Home } from "./Pages";
+import { Admin } from "./Pages/Admin";
 import { Login } from "./Pages/Login";
 import { SendEmbed } from "./Pages/SendEmbed";
 import { SendMessage } from "./Pages/SendMessage";
@@ -17,6 +17,7 @@ function App() {
         <main>
           <Routes>
             <Route element={<RequireAuth />}>
+              <Route path="/admin" element={<Admin />} />
               <Route path="/admin/send-message" element={<SendMessage />} />
               <Route path="/admin/send-embed" element={<SendEmbed />} />
             </Route>
@@ -25,7 +26,6 @@ function App() {
           </Routes>
         </main>
       </Container>
-      <Footer />
     </Router>
   );
 }
