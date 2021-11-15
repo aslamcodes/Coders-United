060647301369cba8089b6b7aad5233d29ccf5a08
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-15 10:02:30 +0530

    Using cards

--- coders-united-interface/src/Pages/Home.tsx
+++ coders-united-interface/src/Pages/Home.tsx
@@ -1,15 +1,8 @@
 import { Link } from "react-router-dom";
 import { Card } from "../Components/Helpers/Card";
-import styles from "./Home.module.css";
+import { Cards } from "../Components/Helpers/Cards";
 
 const HOMEPAGECARDS = [
-  // { href: "admin/send-message", title: "Send a message", isProtected: true },
-  // {
-  //   href: "admin/send-embed",
-  //   title: "Embeds",
-  //   image: "/icons/embed.svg",
-  //   desc: "Send embeds to the server",
-  // },
   {
     href: "contribute",
     title: "Contribute",
@@ -20,18 +13,18 @@ const HOMEPAGECARDS = [
     href: "stats",
     title: "Stats",
     image: "/icons/stats.svg",
-    desc: "See how the server is going, trend and informations",
+    desc: "See how the server is going, trend and information",
   },
 ];
 
 export const Home = () => {
   return (
-    <div className={styles.cards}>
+    <Cards>
       {HOMEPAGECARDS.map(({ href, title, image, desc }) => (
         <Link key={href} to={href}>
           <Card title={title} icon={image} desc={desc} />
         </Link>
       ))}
-    </div>
+    </Cards>
   );
 };
