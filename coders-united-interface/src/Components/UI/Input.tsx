20c0cc7a9fa23fbd2b062fe6b2354fa73f8ab193
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-17 13:57:19 +0530

    Remove useless components

--- coders-united-interface/src/Components/UI/Input.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import styles from "./Input.module.css";
-export const Input = ({ ...props }) => (
-  <input className={styles.input} {...props} />
-);
--- coders-united-interface/src/Components/UI/Label.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "./Label.module.css";
-export const Label = ({ children, ...props }) => (
-  <label className={styles.label} {...props}>
-    {children}
-  </label>
-);
