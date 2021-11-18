2a730ebe3ddc711b895450214cb64a950f1d3160
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-19 00:53:09 +0530

    Route for file upload

--- coders-united-interface/src/App.tsx
+++ coders-united-interface/src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "./Components/Helpers/Container";
 import { Navbar } from "./Components/UI/Navbar";
 import { Roles } from "./Components/UI/Roles";
 import { SendEmbedForm } from "./Components/UI/SendEmbedForm";
+import { SendFile } from "./Components/UI/SendFile";
 import { SendMessageForm } from "./Components/UI/SendMessageForm";
 import { Home } from "./Pages";
 import { Admin } from "./Pages/Admin";
@@ -22,6 +23,7 @@ function App() {
               <Route path="/admin/roles" element={<Roles />} />
               <Route path="/admin/send-message" element={<SendMessageForm />} />
               <Route path="/admin/send-embed" element={<SendEmbedForm />} />
+              <Route path="/admin/send-file" element={<SendFile />} />
             </Route>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
