4da9c68e1ae8babe8f1ad5c58885f59c982160b2
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-19 00:53:30 +0530

    send File component

---
+++ coders-united-interface/src/Components/UI/SendFile.tsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useAuthContext } from "../../Context/Authentication/AuthContext";
+import useChannels from "../../hooks/useChannels";
+import { Button } from "./Button";
+import Form from "./Form";
+import { getChannelOptions } from "./SendMessageForm";
+
+export const SendFile = () => {
+  const [file, setFile] = useState(null);
+  const [message, setMessage] = useState("");
+  const [selectedChannel, setSeletedChannel] = useState(null);
+  const { channels } = useChannels();
+
+  const { user } = useAuthContext();
+  const onFileChangeHandler = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const onMessageChangeHandler = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onSelectedChannelChangeHandler = ({ value }) => {
+    setSeletedChannel(value);
+  };
+  const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("message", message);
+    formData.append("channelId", selectedChannel);
+    try {
+      await axios.post("/channels/file-upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Form onSubmit={submitHandler}>
+      <Form.Group>
+        <Form.Title>Send a File</Form.Title>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Your message</Form.Label>
+        <Form.Input value={message} onChange={onMessageChangeHandler} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Select a channel</Form.Label>
+        <Form.Select
+          options={getChannelOptions(channels)}
+          onChange={onSelectedChannelChangeHandler}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Select a file from your pc</Form.Label>
+        <Form.FileSelect onChange={onFileChangeHandler} />
+      </Form.Group>
+      <Form.Group>
+        <Button type="submit">Send</Button>
+      </Form.Group>
+    </Form>
+  );
+};
