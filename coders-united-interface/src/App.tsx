1aee258f8b808cfb0f165b84f2c07f46882a4294
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-18 20:11:44 +0530

    use the forms directly

--- coders-united-interface/src/App.tsx
+++ coders-united-interface/src/App.tsx
@@ -3,11 +3,11 @@ import "./App.css";
 import { RequireAuth } from "./Components/Auth/RequireAuth";
 import { Container } from "./Components/Helpers/Container";
 import { Navbar } from "./Components/UI/Navbar";
+import { SendEmbedForm } from "./Components/UI/SendEmbedForm";
+import { SendMessageForm } from "./Components/UI/SendMessageForm";
 import { Home } from "./Pages";
 import { Admin } from "./Pages/Admin";
 import { Login } from "./Pages/Login";
-import { SendEmbed } from "./Pages/SendEmbed";
-import { SendMessage } from "./Pages/SendMessage";
 
 function App() {
   return (
@@ -18,8 +18,8 @@ function App() {
           <Routes>
             <Route element={<RequireAuth />}>
               <Route path="/admin" element={<Admin />} />
-              <Route path="/admin/send-message" element={<SendMessage />} />
-              <Route path="/admin/send-embed" element={<SendEmbed />} />
+              <Route path="/admin/send-message" element={<SendMessageForm />} />
+              <Route path="/admin/send-embed" element={<SendEmbedForm />} />
             </Route>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
--- coders-united-interface/src/Pages/SendMessage.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { SendMessageForm } from "../Components/UI/SendMessageForm";
-export const SendMessage = () => {
-  return <SendMessageForm />;
-};
