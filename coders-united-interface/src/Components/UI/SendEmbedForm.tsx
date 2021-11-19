102543bccbd2c54461faed16f9851b6353d1fa2a
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-19 20:53:19 +0530

    Reducer for File

--- coders-united-interface/src/Components/UI/SendEmbedForm.tsx
+++ coders-united-interface/src/Components/UI/SendEmbedForm.tsx
@@ -2,11 +2,15 @@ import React, { useState, useReducer } from "react";
 import { useAuthContext } from "../../Context/Authentication/AuthContext";
 import useAxiosWithCallback from "../../hooks/useAxiosWithCallback";
 import useChannels from "../../hooks/useChannels";
+import { getOptionsFor } from "../../utils/utils";
+import { Button } from "./Button";
+import Form from "./Form";
 
 const embedInitialState = {
   title: "",
   description: "",
   url: "",
+  file: "",
   authorName: "",
   authorUrl: "",
   contentText: "",
@@ -26,163 +30,118 @@ const embedReducer = (state, action) => {
       return { ...state, authorUrl: action.payload };
     case "SET_CONTENT_TEXT":
       return { ...state, contentText: action.payload };
+    case "SET_FILE":
+      return { ...state, file: action.payload };
     default:
       break;
   }
 };
 
 export const SendEmbedForm = () => {
-  const {
-    isLoading: isFetchLoading,
-    error: isErrorInFetching,
-    fetchData: sendEmbed,
-  } = useAxiosWithCallback();
-  const {
-    isLoading: isChannelsLoading,
-    channels,
-    error: isErrorInChannel,
-  } = useChannels();
+  const [embedItems, dispatch] = useReducer(embedReducer, embedInitialState);
+  const { fetchData: sendEmbed } = useAxiosWithCallback();
+  const { channels } = useChannels();
   const [selectedChannel, setSelectedChannel] = useState();
   const { user } = useAuthContext();
-  const onSelectedChannelChangeHandler = (e) => {
-    setSelectedChannel(e.target.value);
+
+  const onSelectedChannelChangeHandler = ({ value }) => {
+    setSelectedChannel(value);
   };
+
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append(
+      "embed",
+      JSON.stringify({
+        title: embedItems.title,
+        description: embedItems.description,
+        url: embedItems.url,
+        authorName: embedItems.authorName,
+        authorUrl: embedItems.authorUrl,
+        contentText: embedItems.contentText,
+      })
+    );
+    formData.append("file", embedItems.file);
+    formData.append("channelId", selectedChannel);
     await sendEmbed({
       method: "POST",
       url: "/channels/embed",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "multipart/form-data",
         Authorization: "Bearer " + user.token,
       },
-      data: {
-        embed: {
-          title: embedItems.title,
-          description: embedItems.description,
-          url: embedItems.url,
-          authorName: embedItems.authorName,
-          authorUrl: embedItems.authorUrl,
-          contentText: embedItems.contentText,
-        },
-        channelId: selectedChannel,
-      },
+      data: formData,
     });
   };
 
-  const [embedItems, dispatch] = useReducer(embedReducer, embedInitialState);
-
+  const onSelectedFileChangeHandler = (e) => {
+    dispatch({ type: "SET_FILE", payload: e.target.files[0] });
+  };
   const onTitleChangeHandler = (e) => {
     dispatch({ type: "SET_TITLE", payload: e.target.value });
   };
   const onDescriptionChangeHandler = (e) => {
     dispatch({ type: "SET_DESCRIPTION", payload: e.target.value });
   };
-  const onUrlChangeHandler = (e) => {
-    dispatch({ type: "SET_URL", payload: e.target.value });
-  };
+
   const onAuthorNameChangeHandler = (e) => {
     dispatch({ type: "SET_AUTHOR_NAME", payload: e.target.value });
   };
-  const onAuthorUrlChangeHandler = (e) => {
-    dispatch({ type: "SET_AUTHOR_URL", payload: e.target.value });
-  };
-  const onContentTextChangeHandler = (e) => {
-    dispatch({ type: "SET_CONTENT_TEXT", payload: e.target.value });
-  };
 
   return (
-    <form onSubmit={onSubmitHandler}>
-      <div className="embed-box">
-        <div className="embed-content">
-          <div className="embed-item">
-            <h4>POST IN CHANNEL</h4>
-            <select id="channel" onChange={onSelectedChannelChangeHandler}>
-              {channels.map(({ id, name }, idx) => {
-                return (
-                  <option key={`id-${id}${name}${idx}`} value={id}>
-                    {name}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div className="embed-item">
-            <h4>DESCRIPTION</h4>
-            <input
-              type="text"
-              onChange={onDescriptionChangeHandler}
-              placeholder="insert a short description"
-              value={embedItems.description}
-            />
-          </div>
-          <div className="embed-item">
-            <h4>EMBED</h4>
-            <label htmlFor="color-picker">COLOR:</label>
-            <input type="color" id="color-picker" />
+    <Form onSubmit={onSubmitHandler}>
+      <Form.Group>
+        <Form.Title>Send a Embed to channel</Form.Title>
+        <Form.Label>Select a channel</Form.Label>
+        <Form.Select
+          options={getOptionsFor(channels)}
+          onChange={onSelectedChannelChangeHandler}
+        />
+      </Form.Group>
 
-            <div className="embed-author">
-              <div className="embed-author-item">
-                <h6>Author image</h6>
-                <input
-                  type="file"
-                  accept="image/gif, image/jpeg, image/jpg, image/png"
-                  style={{ display: "" }}
-                />
-              </div>
-              <div className="embed-author-item">
-                <h6>Author name</h6>
-                <input
-                  type="text"
-                  onChange={onAuthorNameChangeHandler}
-                  placeholder="Author name"
-                  value={embedItems.authorName}
-                />
-              </div>
-              <div className="embed-author-item">
-                <h6>Author url</h6>
-                <input
-                  type="url"
-                  onChange={onAuthorUrlChangeHandler}
-                  placeholder="Author url"
-                  value={embedItems.authorUrl}
-                />
-              </div>
-            </div>
+      <Form.Group>
+        <Form.Label>Color</Form.Label>
+        <Form.ColorInput />
+      </Form.Group>
 
-            <div className="embed-title">
-              <div className="embed-title-item">
-                <h6>Title text</h6>
-                <input
-                  type="text"
-                  onChange={onTitleChangeHandler}
-                  placeholder="Title text"
-                  value={embedItems.title}
-                />
-              </div>
-              <div className="embed-title-item">
-                <h6>Title url</h6>
-                <input
-                  type="text"
-                  onChange={onUrlChangeHandler}
-                  placeholder="Title url"
-                  value={embedItems.url}
-                />
-              </div>
-              <div className="embed-title-item">
-                <h6>Content text</h6>
-                <textarea
-                  name="embed-content-text"
-                  onChange={onContentTextChangeHandler}
-                  value={embedItems.contentText}
-                ></textarea>
-              </div>
-            </div>
-          </div>
+      <Form.Group>
+        <Form.FileSelect
+          title={embedItems.file?.name || "Upload image"}
+          onChange={onSelectedFileChangeHandler}
+        />
+      </Form.Group>
 
-          <button type="submit">Send an embed</button>
-        </div>
-      </div>
-    </form>
+      <Form.Group>
+        <Form.Label>Author name</Form.Label>
+        <Form.Input
+          type="text"
+          onChange={onAuthorNameChangeHandler}
+          placeholder="Author name"
+          value={embedItems.authorName}
+        />
+
+        <Form.Group>
+          <Form.Label>Title text</Form.Label>
+          <Form.Input
+            onChange={onTitleChangeHandler}
+            placeholder="Title text"
+            value={embedItems.title}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Content text</Form.Label>
+          <Form.TextArea
+            name="embed-content-text"
+            onChange={onDescriptionChangeHandler}
+            value={embedItems.description}
+          ></Form.TextArea>
+        </Form.Group>
+      </Form.Group>
+      <Form.Group>
+        <Button type="submit">Send an embed</Button>
+      </Form.Group>
+    </Form>
   );
 };
