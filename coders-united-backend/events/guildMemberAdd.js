398b1e6f78472ee9d61ac62e453d34dccbd56c37
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-11-24 13:14:33 +0530

    Add avatar to embed

--- coders-united-backend/events/guildMemberAdd.js
+++ coders-united-backend/events/guildMemberAdd.js
@@ -3,17 +3,27 @@ const Discord = require("discord.js");
 module.exports = {
   name: "guildMemberAdd",
   async execute(interaction) {
+    if (interaction.user.bot) return;
     const welcomeEmbed = new Discord.MessageEmbed()
       .setColor("#0099ff")
       .setTitle("Welcome to the Coders United Discord!")
-      .setDescription("Have a great time here!");
+      .setDescription("Have a great time here!")
+      .setThumbnail(`attachment://logo.png`)
+      .setImage(interaction.user.displayAvatarURL());
+
     const channel = await interaction.guild.channels.cache.get(
-      "846477643058642985"
+      "859384213349728291"
     );
     channel.sendTyping();
     await channel.send({
       content: `Welcome <@${interaction.user.id}>`,
       embeds: [welcomeEmbed],
+      files: [
+        {
+          attachment: `./coders-united-backend/data/uploads/logo.png`,
+          name: "logo.png",
+        },
+      ],
     });
   },
 };
